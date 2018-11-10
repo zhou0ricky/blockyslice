a23167498df53133617bdd480b539590cc596065
zhou0ricky <zhou0ricky@gmail.com>
2018-11-10 01:05:00 -0500

    changes

--- main.py
+++ main.py
@@ -6,184 +6,185 @@ import copy
 gravity = 0.1
 
 def intersect(line1, line2):
-	xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
-	ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
 
-	def det(a, b):
-		return a[0] * b[1] - a[1] * b[0]
+    def det(a, b):
+	return a[0] * b[1] - a[1] * b[0]
 
-	div = det(xdiff, ydiff)
-	if div == 0:
-	   return None
+    div = det(xdiff, ydiff)
+    if div == 0:
+	return None
 
-	d = (det(*line1), det(*line2))
-	x = det(d, xdiff) / div
-	y = det(d, ydiff) / div
-	if (x >= min(line1[0][0], line1[1][0]) and
-		x >= min(line2[0][0], line2[1][0]) and
-		x <= max(line1[0][0], line1[1][0]) and
-		x <= max(line2[0][0], line2[1][0]) and
-		y >= min(line1[0][1], line1[1][1]) and
-		y >= min(line2[0][1], line2[1][1]) and
-		y <= max(line1[0][1], line1[1][1]) and
-		y <= max(line2[0][1], line2[1][1])):
-		return [x, y]
+    d = (det(*line1), det(*line2))
+    x = det(d, xdiff) / div
+    y = det(d, ydiff) / div
+    if (x >= min(line1[0][0], line1[1][0]) and
+	x >= min(line2[0][0], line2[1][0]) and
+	x <= max(line1[0][0], line1[1][0]) and
+	x <= max(line2[0][0], line2[1][0]) and
+	y >= min(line1[0][1], line1[1][1]) and
+	y >= min(line2[0][1], line2[1][1]) and
+	y <= max(line1[0][1], line1[1][1]) and
+	y <= max(line2[0][1], line2[1][1])):
+	return [x, y]
 
 def transformPoints(points, position, rotation):
-	newPoints = []
-	c = math.cos(rotation)
-	s = math.sin(rotation)
-	for point in points:
-		x = point[0]
-		y = point[1]
-		rx = x * c - y * s
-		ry = y * c + x * s
-		px = rx + position[0]
-		py = ry + position[1]
-		newPoints.append([px, py])
-	return newPoints
+    newPoints = []
+    c = math.cos(rotation)
+    s = math.sin(rotation)
+    for point in points:
+	x = point[0]
+	y = point[1]
+	rx = x * c - y * s
+	ry = y * c + x * s
+	px = rx + position[0]
+	py = ry + position[1]
+	newPoints.append([px, py])
+    return newPoints
 
 def invertPoints(points, position, rotation):
-	newPoints = []
-	c = math.cos(-rotation)
-	s = math.sin(-rotation)
-	for point in points:
-		x = point[0]
-		y = point[1]
-		px = x - position[0]
-		py = y - position[1]
-		rx = px * c - py * s
-		ry = py * c + px * s
-		newPoints.append([rx, ry])
-	return newPoints
+    newPoints = []
+    c = math.cos(-rotation)
+    s = math.sin(-rotation)
+    for point in points:
+	x = point[0]
+	y = point[1]
+	px = x - position[0]
+	py = y - position[1]
+	rx = px * c - py * s
+	ry = py * c + px * s
+	newPoints.append([rx, ry])
+    return newPoints
 
 
 class Polygon(object):
-	def __init__(self, points, position, velocity, rotation, rotationRate):
-		self.points = points
-		self.position = position
-		self.velocity = velocity
-		self.rotation = rotation
-		self.rotationRate = rotationRate
+    def __init__(self, points, position, velocity, rotation, rotationRate):
+	self.points = points
+	self.position = position
+	self.velocity = velocity
+	self.rotation = rotation
+	self.rotationRate = rotationRate
 
-	def recenter(self):
-		center = [0, 0]
-		for point in self.points:
-			center[0] += point[0]
-			center[1] += point[1]
-		center[0] /= len(self.points)
-		center[1] /= len(self.points)
-		for point in self.points:
-			point[0] -= center[0]
-			point[1] -= center[1]
-		self.position[0] -= center[0]
-		self.position[1] -= center[1]
+    def recenter(self):
+	center = [0, 0]
+	for point in self.points:
+	    center[0] += point[0]
+	    center[1] += point[1]
+	center[0] /= len(self.points)
+	center[1] /= len(self.points)
+	for point in self.points:
+	    point[0] -= center[0]
+	    point[1] -= center[1]
+	self.position[0] -= center[0]
+	self.position[1] -= center[1]
 
-	def slice(self, line, polygons):
-		newPoints = transformPoints(self.points, self.position, self.rotation)
-		intersectionPoints = []
-		intersectionEdgeNums = []
-		for edgeNum in range(0, len(newPoints)):
-			edge = [newPoints[edgeNum], newPoints[(edgeNum + 1) % len(newPoints)]]
-			inter = intersect(edge, line)
-			if inter != None:
-				intersectionPoints.append(inter)
-				intersectionEdgeNums.append(edgeNum)
-		if len(intersectionPoints) == 2:
-			intersectionPoints = invertPoints(intersectionPoints, self.position, self.rotation)
-			half1 = [intersectionPoints[0]]
-			half2 = [intersectionPoints[1]]
-			for i in range(0, len(newPoints)):
-				if intersectionEdgeNums[0] < i and i <= intersectionEdgeNums[1]:
-					half1.append(self.points[i])
-				else:
-					half2.append(self.points[i])
-			half1.append(intersectionPoints[1])
-			half2.append(intersectionPoints[0])
-			self.points = half1
-			smaller = Polygon(half2, copy.copy(self.position), copy.copy(self.velocity), self.rotation, self.rotationRate)
-			polygons.append(smaller)
+    def slice(self, line, polygons):
+	newPoints = transformPoints(self.points, self.position, self.rotation)
+	intersectionPoints = []
+	intersectionEdgeNums = []
+	    for edgeNum in range(0, len(newPoints)):
+		edge = [newPoints[edgeNum], newPoints[(edgeNum + 1) % len(newPoints)]]
+		inter = intersect(edge, line)
+		if inter != None:
+		    intersectionPoints.append(inter)
+		    intersectionEdgeNums.append(edgeNum)
+	    if len(intersectionPoints) == 2:
+		intersectionPoints = invertPoints(intersectionPoints, self.position, self.rotation)
+		half1 = [intersectionPoints[0]]
+		half2 = [intersectionPoints[1]]
+		for i in range(0, len(newPoints)):
+		    if intersectionEdgeNums[0] < i and i <= intersectionEdgeNums[1]:
+			half1.append(self.points[i])
+		    else:
+			half2.append(self.points[i])
+		half1.append(intersectionPoints[1])
+	        half2.append(intersectionPoints[0])
+		self.points = half1
+	        smaller = Polygon(half2, copy.copy(self.position), copy.copy(self.velocity), self.rotation, self.rotationRate)
+		polygons.append(smaller)
 
 
-	def move(self, time):
-		self.position[0] += self.velocity[0] * time
-		self.position[1] += self.velocity[1] * time
-		self.velocity[1] += gravity * time
-		self.rotation += self.rotationRate * time
+    def move(self, time):
+	self.position[0] += self.velocity[0] * time
+	self.position[1] += self.velocity[1] * time
+	self.velocity[1] += gravity * time
+	self.rotation += self.rotationRate * time
 
 def createPolygon(screenSize):
-	points = [
-		[-50, -50], [-50, 50],
-		[50, 50], [50, -50]
-	]
+    points = [
+	[-50, -50], [-50, 50],
+	[50, 50], [50, -50]
+    ]
 
-	posX = random.randint(0, screenSize[0])
-	toTravel = screenSize[0] / 2 - posX
-	posY = screenSize[1]
-	velY = -10
-	velX = toTravel / (-velY / gravity)
+    posX = random.randint(0, screenSize[0])
+    toTravel = screenSize[0] / 2 - posX
+    posY = screenSize[1]
+    velY = -10
+    velX = toTravel / (-velY / gravity)
 
-	position = [posX, posY]
-	velocity = [velX, velY]
-	rotation = 0.05
-	rotationRate = random.randint(-10, 10) / 50
-	return Polygon(points, position, velocity, rotation, rotationRate)
+    position = [posX, posY]
+    velocity = [velX, velY]
+    rotation = 0.05
+    rotationRate = random.randint(-10, 10) / 50
+    return Polygon(points, position, velocity, rotation, rotationRate)
+
+
 
 def main():
-	pygame.init()
-	screenSize = [800, 600]
+    pygame.init()
+    screenSize = [800, 600]
 
-	screen = pygame.display.set_mode(screenSize)
-	clock = pygame.time.Clock()
+    screen = pygame.display.set_mode(screenSize)
+    clock = pygame.time.Clock()
 
-	pygame.display.set_caption("Blocky Slice")
+    pygame.display.set_caption("Blocky Slice")
 
-	running = True
+    running = True
 
-	time = 1
+    time = 1
 
-	polygon = createPolygon(screenSize)
-	polygons = [polygon]
+    polygon = createPolygon(screenSize)
+    polygons = [polygon]
 
-	cutting = False
-	firstCut = []
-	secondCut = []
+    cutting = False
+    firstCut = []
+    secondCut = []
 
-	while(running):
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				running = False
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_ESCAPE:
-					running = False
-			if event.type == pygame.MOUSEBUTTONDOWN:
-				firstCut = pygame.mouse.get_pos()
-				cutting = True
-			elif event.type == pygame.MOUSEBUTTONUP:
-				cutting = False
-				polygon.slice([firstCut, secondCut], polygons);
-		secondCut = pygame.mouse.get_pos()
+    while(running):
+        for event in pygame.event.get():
+	    if event.type == pygame.QUIT:
+		running = False
+	    if event.type == pygame.KEYDOWN:
+		if event.key == pygame.K_ESCAPE:
+		    running = False
+	    if event.type == pygame.MOUSEBUTTONDOWN:
+		firstCut = pygame.mouse.get_pos()
+		cutting = True
+	    elif event.type == pygame.MOUSEBUTTONUP:
+		cutting = False
+		polygon.slice([firstCut, secondCut], polygons);
+	secondCut = pygame.mouse.get_pos()
 
-		clock.tick(60)
-		screen.fill([255, 255, 255])
+	clock.tick(60)
+	screen.fill([255, 255, 255])
 		
-		for poly in polygons:
-			pygame.gfxdraw.filled_polygon(screen, transformPoints(poly.points, poly.position, poly.rotation), [0, 0, 0])
-			poly.move(time)
+	for poly in polygons:
+	    pygame.gfxdraw.filled_polygon(screen, transformPoints(poly.points, poly.position, poly.rotation), [0, 0, 0])
+	    poly.move(time)
 
-		if cutting:
-			pygame.gfxdraw.line(screen, firstCut[0], firstCut[1], 
-								secondCut[0], secondCut[1], [0, 0 ,0])
-			time = (0.1 + time) / 2
-		else:
-			time = (1 + time) / 2
+	if cutting:
+	    pygame.gfxdraw.line(screen, firstCut[0], firstCut[1], secondCut[0], secondCut[1], [0, 0 ,0])
+	    time = (0.1 + time) / 2
+	else:
+	    time = (1 + time) / 2
 
-		if polygon.position[1] > screenSize[1]:
-			polygon = createPolygon(screenSize)
-			polygons = [polygon]
+	if polygon.position[1] > screenSize[1]:
+	    polygon = createPolygon(screenSize)
+	    polygons = [polygon]
 
-		pygame.display.flip()
+	pygame.display.flip()
 
-	pygame.quit()
+    pygame.quit()
 
 main()
